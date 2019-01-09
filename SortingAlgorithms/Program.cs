ef9446a4f29cfaa11f2dc0a259d5bf35185e5cd2
Ben LaPaz <bdl@lifestylestaffing.com>
2019-01-09 15:38:40 -0600

    Create function to populate int array with random numbers

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -7,20 +7,50 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
+            // Create new array filled with random values (seeded)
+            int[] values = NewIntArray(15, 1);
+            // Print to console the unsorted array
+            Console.WriteLine("Unsorted: " + string.Join(",", values));
+
+            // Start stopwatch
             Stopwatch watch = Stopwatch.StartNew();
 
-            int[] values = new int[]
-            {
-                9, 8, 7, 6, 5, 6, 4, 3, 2, 1, 0, 2
-            };
-
+            // Sort using selected sort method 
             Sort sort = new Sort();
             int[] sorted = sort.Comb(values);
 
+            // Stop stopwatch
             watch.Stop();
 
-            Console.WriteLine("[Elapsed: " + watch.Elapsed + "] " + string.Join(",", sorted));
+            // Print to console time elapsed and sorted array
+            Console.WriteLine("Sorted:   " + string.Join(",", sorted));
+            Console.WriteLine("Elapsed:  " + watch.Elapsed);
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Creates int array populated with random values
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="seed"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        static int[] NewIntArray(int size, int seed = 0, int max = 15)
+        {
+            int[] values = new int[size];
+
+            if (seed == 0)
+            {
+                seed = Guid.NewGuid().GetHashCode();
+            }
+            Random rnd = new Random(seed);
+
+            for (int i = 0; i < size; i++)
+            {
+                values[i] = rnd.Next(max);
+            }
+
+            return values;
         }
     }
 }
