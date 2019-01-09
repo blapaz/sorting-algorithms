9b3cdc3a70f2e23ab54f76751d30c2376c452fe3
Ben LaPaz <bdl@lifestylestaffing.com>
2019-01-09 13:44:14 -0600

    Add stopwatch to track execution time of algorithms

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SortingAlgorithms
 {
@@ -6,15 +7,19 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
+            Stopwatch watch = Stopwatch.StartNew();
+
             int[] values = new int[]
             {
                 9, 8, 7, 6, 5, 6, 4, 3, 2, 1, 0, 10
             };
 
             Sort sort = new Sort();
-            int[] sorted = sort.Reverse(sort.Merge(values));
+            int[] sorted = sort.Merge(values);
 
-            Console.WriteLine(string.Join(",", sorted));
+            watch.Stop();
+
+            Console.WriteLine("[Elapsed: " + watch.Elapsed + "] " + string.Join(",", sorted));
             Console.ReadLine();
         }
     }
